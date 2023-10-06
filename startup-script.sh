e1a7e9314d1507bc27c11e199590b5f8688bbbaa
OmarHaka <107427573+OmarHaka@users.noreply.github.com>
2023-10-06 16:42:31 -0500

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -16,15 +16,15 @@ git clone https://github.com/OmarHaka/pythonHWapp.git /opt/app
 useradd -m -d /home/pythonapp pythonapp
 
 # Python environment setup
-virtualenv -p python3 /opt/app/env
-/bin/bash -c "source /opt/app/env/bin/activate"
-/opt/app/gce/env/bin/pip install -r /opt/app/requirements.txt
+virtualenv -p python3 /opt/app/gce/env
+/bin/bash -c "source /opt/app/gce/env/bin/activate"
+/opt/app/gce/env/bin/pip install -r /opt/app/gce/requirements.txt
 
 # Set ownership to newly created account
 chown -R pythonapp:pythonapp /opt/app
 
 # Put supervisor configuration in proper place
-cp /opt/app/python-app.conf /etc/supervisor/conf.d/python-app.conf
+cp /opt/app/gce/python-app.conf /etc/supervisor/conf.d/python-app.conf
 
 # Start service via supervisorctl
 supervisorctl reread
